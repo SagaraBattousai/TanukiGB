95b53cb261df0970894f6a2bf3d75c7a8a3b514f
James Calo <jamesafcalo@gmail.com>
2025-08-05 01:30:26 +0100

    Cleaned Executor CRTP (Replacing Executor Concept) so that the interface is garunteed before a class is finished defining itself.

--- include/tanukigb/utility/crtp.h
+++ include/tanukigb/utility/crtp.h
@@ -5,28 +5,22 @@
 
 namespace tanukigb {
 
-// Cant refer to CU in Crtp (at least the way I was trying
-//template <typename Underlying, template <typename CU> typename CRTP_T>
 template<typename Underlying, template <typename> typename CRTP_T>
 class Crtp {
  public:
-  Underlying& to_underlying() noexcept {
-    return static_cast<Underlying&>(*this);
+  using underlying_type = Underlying;
+
+  underlying_type& to_underlying() noexcept {
+    return static_cast<underlying_type&>(*this);
   }
 
-  const Underlying& to_underlying() const noexcept {
-    return static_cast<const Underlying&>(*this);
+  const underlying_type& to_underlying() const noexcept {
+    return static_cast<const underlying_type&>(*this);
   }
 
  private:
-  Crtp() = default;           /*{
-    static_assert(std::is_same_v<Underlying, typename CU>,
-                  "Crtp base class must have a crtp pattern class as its "
-                  "second parameter and its underlying type must be the same "
-                  "as the template parameter Underlying.")
-  }*/
-  friend CRTP_T<Underlying>;  // Maybe same as static assert as otherwise the
-                              // constructor cannot be called?
+  Crtp() = default;
+  friend CRTP_T<Underlying>;
 };
 
 }  // namespace tanukigb
